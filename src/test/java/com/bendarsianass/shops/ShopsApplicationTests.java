2b7f9abd0032fc664651672f25dcb2d3eeb51316
Anass Bendarsi <bendarsi@gmail.com>
2019-11-09 00:22:21 +0100

    init the project with spring initializr

---
+++ src/test/java/com/bendarsianass/shops/ShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bendarsianass.shops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
