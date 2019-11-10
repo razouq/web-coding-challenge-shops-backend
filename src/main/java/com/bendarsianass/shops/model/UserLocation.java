ecaa618d65df4581a58c8123f7d9025a140451d2
Anass Bendarsi <bendarsi@gmail.com>
2019-11-10 14:24:56 +0100

    Add UserLocation model

---
+++ src/main/java/com/bendarsianass/shops/model/UserLocation.java
@@ -0,0 +1,26 @@
+package com.bendarsianass.shops.model;
+
+public class UserLocation {
+
+    private Double lat;
+    private Double lon;
+
+    public UserLocation() {
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+}
