269446ac32067de09f78e3340257563a6e0667cc
Anass Bendarsi <bendarsi@gmail.com>
2019-11-10 14:29:33 +0100

    Replace lat and lon request param with userLocation requestBody

--- src/main/java/com/bendarsianass/shops/controller/ShopContoller.java
+++ src/main/java/com/bendarsianass/shops/controller/ShopContoller.java
@@ -1,12 +1,10 @@
 package com.bendarsianass.shops.controller;
 
 import com.bendarsianass.shops.entity.Shop;
+import com.bendarsianass.shops.model.UserLocation;
 import com.bendarsianass.shops.service.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,9 +15,9 @@ public class ShopContoller {
     @Autowired
     private ShopService shopService;
 
-    @GetMapping("/all")
-    public List<Shop> all(@RequestParam double lat, @RequestParam double lon, @RequestParam int page) {
-        System.out.println(lat+ " " + lon);
-        return shopService.getAllShops(lat, lon, page);
+    @PostMapping("/all")
+    public List<Shop> all(@RequestBody UserLocation userLocation, @RequestParam int page) {
+        System.out.println(userLocation.getLat()+ " " + userLocation.getLon());
+        return shopService.getAllShops(userLocation.getLat(), userLocation.getLon(), page);
     }
 }
