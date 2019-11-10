30aeb14cc382d7d2a07844d1ed707c6103bffb06
Anass Bendarsi <bendarsi@gmail.com>
2019-11-10 14:46:09 +0100

    Add UserRegistrationRequest model

---
+++ src/main/java/com/bendarsianass/shops/model/UserRegistrationRequest.java
@@ -0,0 +1,35 @@
+package com.bendarsianass.shops.model;
+
+public class UserRegistrationRequest {
+
+    private String username;
+    private String password;
+    private String passwordConfirm;
+
+    public UserRegistrationRequest() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+}
