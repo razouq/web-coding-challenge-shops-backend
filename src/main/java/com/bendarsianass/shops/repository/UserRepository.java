3bb5a300ed38f0778cc708167b7729e73d17ccc1
Anass Bendarsi <bendarsi@gmail.com>
2019-11-10 17:42:27 +0100

    Add user entity and repository

---
+++ src/main/java/com/bendarsianass/shops/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.bendarsianass.shops.repository;
+
+import com.bendarsianass.shops.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
+}
