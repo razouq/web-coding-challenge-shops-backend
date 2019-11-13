e9e61f13077c80de1c6958f6926200dd175fb040
Anass Bendarsi <bendarsi@gmail.com>
2019-11-14 00:41:10 +0100

    add authentication manager to verifier the password and username

--- src/main/java/com/bendarsianass/shops/controller/AuthController.java
+++ src/main/java/com/bendarsianass/shops/controller/AuthController.java
@@ -2,6 +2,11 @@ package com.bendarsianass.shops.controller;
 
 import com.bendarsianass.shops.model.JwtRequest;
 import com.bendarsianass.shops.model.JwtResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/authentication")
 public class AuthController {
 
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @PostMapping("/login")
     public JwtResponse login(@RequestBody JwtRequest jwtRequest) {
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(), jwtRequest.getPassword())
+        );
+
+        // if no exception is generated we will create the token
         return new JwtResponse("token");
     }
 
