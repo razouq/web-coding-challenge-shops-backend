507031548d3eb4b0160a06525e6bc66be2840fbf
Anass Bendarsi <bendarsi@gmail.com>
2019-11-14 00:40:26 +0100

    add h2-console to the allowed endpoints

--- src/main/java/com/bendarsianass/shops/security/SecurityConfig.java
+++ src/main/java/com/bendarsianass/shops/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.bendarsianass.shops.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,8 +11,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final String[] PUBLIC_ENDPOINTS = {
-        "/api/authentication/**"
+            "/api/authentication/login",
+            "/h2-console/**"
     };
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
