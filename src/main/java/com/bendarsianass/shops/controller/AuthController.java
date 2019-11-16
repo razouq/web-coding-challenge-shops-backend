d5204603034091ac774f431ff4e19ac0630bb459
Anass Bendarsi <bendarsi@gmail.com>
2019-11-16 20:19:28 +0100

    fix cross origin in authentication controller

--- src/main/java/com/bendarsianass/shops/controller/AuthController.java
+++ src/main/java/com/bendarsianass/shops/controller/AuthController.java
@@ -10,13 +10,11 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/authentication")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     @Autowired
