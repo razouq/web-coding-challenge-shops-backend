3c6a060eae4daaa7032f1e4ded57c474c7152049
Anass Bendarsi <bendarsi@gmail.com>
2019-11-16 17:51:28 +0100

    cross origin in account controller

--- src/main/java/com/bendarsianass/shops/controller/AccountController.java
+++ src/main/java/com/bendarsianass/shops/controller/AccountController.java
@@ -4,13 +4,11 @@ import com.bendarsianass.shops.entity.UserEntity;
 import com.bendarsianass.shops.model.UserRegistrationRequest;
 import com.bendarsianass.shops.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/account")
+@CrossOrigin(origins = "*")
 public class AccountController {
 
     @Autowired
