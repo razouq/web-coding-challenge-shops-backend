4db30a5e63bb72d4083901622089b15e0f077af4
Anass Bendarsi <bendarsi@gmail.com>
2019-12-01 21:38:42 +0100

    retrieve the current user from token, and add it to the request in the auth filter layer

--- src/main/java/com/bendarsianass/shops/security/AuthFilter.java
+++ src/main/java/com/bendarsianass/shops/security/AuthFilter.java
@@ -1,11 +1,14 @@
 package com.bendarsianass.shops.security;
 
 import com.bendarsianass.shops.Util.TokenUtil;
+import com.bendarsianass.shops.entity.UserEntity;
+import com.bendarsianass.shops.repository.UserRepository;
 import com.bendarsianass.shops.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -26,20 +29,25 @@ public class AuthFilter extends OncePerRequestFilter {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         final String header = request.getHeader(TOKEN_HEADER);
         final SecurityContext securityContext = SecurityContextHolder.getContext();
-        System.out.println("auth filter");
         if(header != null && securityContext.getAuthentication() == null) {
             String token = header.substring("Bearer ".length());
             String username = tokenUtil.getUsernameFromToken(token);
-            System.out.println("header exists " + username);
             if(username != null) {
-                System.out.println("username exists");
+
+                // put the current user in the request
+                Long userId = userService.findUserEntityIdFromToken(token);
+                UserEntity userEntity = userRepository.findById(userId).get();
+                request.setAttribute("currentUser", userEntity);
+
                 UserDetails userDetails = userService.loadUserByUsername(username);
                 if(tokenUtil.isTokenValid(token, userDetails)) {
-                    System.out.println("valid token");
                     UsernamePasswordAuthenticationToken authentication =
                             new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                     authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
